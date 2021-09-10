d6a4144902b97bffbd49e873b135788fc5e464d4
derReloader <62664722+derReloader@users.noreply.github.com>
2021-09-10 13:31:06 +0200

    add feed scroll mechanism

--- newsfeed-frontend/src/app/feed/feed.component.ts
+++ newsfeed-frontend/src/app/feed/feed.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
+  posts: string[] = [
+    'Post 1 ist ein nicer Post',
+    'Post 2 ist kein nicer Post',
+    'Ob du jetzt hier bist oder in China fällt ein Sack Reis um',
+    'Morgenstund hat Gold im Mund',
+    'Der frühe Vogel fängt den Wurm',
+    'Post 5 ist ein nicer Post',
+    'Post 6 ist kein nicer Post'
+  ]
+
+  index = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  createPost() {
+    this.posts.push("NEW ONE " + this.index);
+    this.index++;
+  }
+
+  removeLastPost() {
+    this.posts.pop();
+  }
+
+  removeAll() {
+    this.posts = [];
+  }
 }
