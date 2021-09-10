c127adec6a9711c0a732dbaa9b2c6982cea9ce33
derReloader <62664722+derReloader@users.noreply.github.com>
2021-09-10 17:19:49 +0200

    style feed section + add angular material

--- newsfeed-frontend/src/app/app.module.ts
+++ newsfeed-frontend/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeedComponent } from './feed/feed.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent,
     FeedComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatButtonModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- newsfeed-frontend/src/app/feed/feed.component.ts
+++ newsfeed-frontend/src/app/feed/feed.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FeedComponent implements OnInit {
 
   posts: string[] = [
-    'Post 1 ist ein nicer Post',
-    'Post 2 ist kein nicer Post',
+    'Then on it there are placed two divs: "bottom" and "right" which have height and width of your wish. I set up background red to show in more understandable way how it works. bla bla bla.',
+    'Then on it there are placed two divs: "bottom" and "right" which have height and width of your wish. I set up background red to show in more understandable way how it works.',
     'Ob du jetzt hier bist oder in China fällt ein Sack Reis um',
     'Morgenstund hat Gold im Mund',
     'Der frühe Vogel fängt den Wurm',
