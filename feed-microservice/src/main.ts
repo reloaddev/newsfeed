d172c91e3686a0f4097bfe70adc969447e475b04
derReloader <62664722+derReloader@users.noreply.github.com>
2021-10-02 19:35:50 +0200

    rename be to feed-microservice + clean up feed-microservice structure

--- feed-microservice/src/main.ts
+++ feed-microservice/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { FeedModule } from './feed.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(FeedModule);
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
